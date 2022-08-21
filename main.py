78825e88bac82cc4f19c328597a548374c914914
Paulmue0 <muellerpaul2000@live.de>
2022-08-21 22:10:16 +0200

    minor fixes

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 import rumps
 import time
-import datetime
-# from pynput import keyboard
 
-########
-########
 # TODO
 # + Icon should be white for dark mode
 # + unhide while paused should show time
-# + when hidden and new session starts, bzzz! should go away
 # + clicking on notification should start new timer
 # + custom time
 # + show in which pomodoro your're in (long break resets)
-########
-########
+
+
 rumps.debug_mode(True)
 
 current_mode = ''
 
-pomodoro_minutes = 25
-pomodoro_seconds = 0
+POMODORO_MINUTES = 25
+POMODORO_SECONDS = 0
 
-long_break_minutes = 15
-long_break_seconds = 0
+LONG_BREAK_MINUTES = 15
+LONG_BREAK_SECONDS = 0
 
-short_break_minutes = 5
-short_break_seconds = 0
+SHORT_BREAK_MINUTES = 5
+SHORT_BREAK_SECONDS = 0
 
 time_hidden = False
 notification_enabled = False
@@ -69,6 +64,7 @@ def a(sender):
 
 def start_timer(_):
     pomodoro_timer.start()
+    app.title = ""
 
 
 def end_timer():
@@ -85,7 +81,7 @@ def end_timer():
 def start_pomodoro(_):
     global current_mode
     current_mode = 'pomodoro'
-    update_time(pomodoro_minutes, pomodoro_seconds)
+    update_time(POMODORO_MINUTES, POMODORO_SECONDS)
     start_timer(1)
 
 
@@ -93,7 +89,7 @@ def start_pomodoro(_):
 def start_short_pause(_):
     global current_mode
     current_mode = 'short pause'
-    update_time(short_break_minutes, short_break_seconds)
+    update_time(SHORT_BREAK_MINUTES, SHORT_BREAK_SECONDS)
     start_timer(1)
 
 
@@ -101,7 +97,7 @@ def start_short_pause(_):
 def start_long_pause(_):
     global current_mode
     current_mode = 'long pause'
-    update_time(long_break_minutes, long_break_seconds)
+    update_time(LONG_BREAK_MINUTES, LONG_BREAK_SECONDS)
     start_timer(1)
 
 
